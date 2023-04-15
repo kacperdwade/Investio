b48ed77d793afc831ed1d58b32433927c23ef6d0
kacper <kacperlbj@gmail.com>
2023-04-15 18:26:33 +0200

    AddInvestment - initial working version

--- investio/main/models.py
+++ investio/main/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Investment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="investment", null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist", null=True)
     name = models.CharField(max_length=50)
     img = models.CharField(max_length=200)
     location = models.CharField(max_length=50)
--- investio/main/views.py
+++ investio/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import AddNewInvestment
-from .models import Investment
+
 # Create your views here.
 def home(request):
     context= {
@@ -9,22 +9,8 @@ def home(request):
     return render(request, 'home.html', context)
 
 def addInvestment(request):
-    form = AddNewInvestment()
-    if request.method == 'POST':
-        form = AddNewInvestment(request.POST)
-        if form.is_valid():
-            new_investment = Investment(
-                name=form.cleaned_data['name'],
-                img=form.cleaned_data['img'],
-                location=form.cleaned_data['location'],
-                about=form.cleaned_data['about'],
-                price=form.cleaned_data['price'],
-                ror=form.cleaned_data['ror'],
-                tor=form.cleaned_data['tor'],
-            )
-            new_investment.save()
-            request.user.investment.add(new_investment)
-    context = {
+    form = AddNewInvestment(request.POST)
+    context= {
         'user': request.user,
         'form': form,
     }
