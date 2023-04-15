655f9637790dcc020a9ecf2c6dc58307740edc28
kacper <kacperlbj@gmail.com>
2023-04-15 18:15:59 +0200

    AddInvestment - initial working version

--- investio/main/views.py
+++ investio/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import AddNewInvestment
-from models import Investment
+from .models import Investment
 # Create your views here.
 def home(request):
     context= {
@@ -9,19 +9,21 @@ def home(request):
     return render(request, 'home.html', context)
 
 def addInvestment(request):
+    form = AddNewInvestment()
     if request.method == 'POST':
         form = AddNewInvestment(request.POST)
         if form.is_valid():
             new_investment = Investment(
-                name=form.name,
-                img=form.img,
-                location=form.location,
-                about=form.about,
-                price=form.price,
-                ror=form.ror,
-                tor=form.tor,
+                name=form.cleaned_data['name'],
+                img=form.cleaned_data['img'],
+                location=form.cleaned_data['location'],
+                about=form.cleaned_data['about'],
+                price=form.cleaned_data['price'],
+                ror=form.cleaned_data['ror'],
+                tor=form.cleaned_data['tor'],
             )
-
+            new_investment.save()
+            request.user.investment.add(new_investment)
     context = {
         'user': request.user,
         'form': form,
