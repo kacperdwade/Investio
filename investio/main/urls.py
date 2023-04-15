ca3305a7874f17ccc18f5a2cc150bfc78def34db
Majki <m.ociepka97@gmail.com>
2023-04-15 16:46:54 +0200

    navbar update

--- investio/main/urls.py
+++ investio/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path('', views.home, name='home'),
-    path('addInvestment/', views.addInvestment, name='Add_Investment'),
+    path('addInvestment/', views.addInvestment, name='add_investment'),
 ]
